a377b2eed5bcbe121b1fb2d7128dcc8de647f678
Alex Steiner <65250433+AlexSteiner30@users.noreply.github.com>
2023-07-19 11:48:58 +0200

    Update models.py

---
+++ models.py
@@ -0,0 +1,27 @@
+import torch
+import torch.nn as nn
+
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.main = nn.Sequential(
+            nn.Conv1d(128, 64, 1, bias=False),
+            nn.LeakyReLU(negative_slope=0.2),
+
+            nn.Conv1d(128, 64, 1, bias=False),
+            nn.LeakyReLU(negative_slope=0.2),
+
+            nn.Conv1d(64, 32, 1, bias=False),
+            nn.LeakyReLU(negative_slope=0.2),
+
+            nn.MaxPool1d(kernel_size=(2, 2)),
+            nn.Flatten(),
+
+            nn.Linear(32, 16),
+            nn.LeakyReLU(negative_slope=0.2),
+
+            nn.Linear(16, 8)
+        )
+    def forward(self, x):
+        x = self.main()
+        return x
